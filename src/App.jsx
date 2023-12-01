9c936e85e7fe8b41bfbe0391baa28bd8d17fb563
luisdevelop3r <luisvalientedeveloper@gmail.com>
2023-12-01 09:58:54 -0500

    Primer commit con la plantilla default de react en vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App

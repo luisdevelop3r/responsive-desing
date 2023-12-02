ec9006d9b97eb2bff17b674a6fc1a83c3db80655
luisdevelop3r <luisvalientedeveloper@gmail.com>
2023-12-02 11:59:15 -0500

    Header de la landing page completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import logo from './assets/img/logo.svg'
+
 function App() {
   return (
     <>
-      <header></header>
+      <header>
+        <img src={logo} alt="logoimg" />
+        <div className="header--title-container">
+          <h1>La próxima revolución en el intercambio de criptomonedas.</h1>
+          <p>Batatabit te ayuda a navegar entre los diferentes precios y tendencias.</p>
+          <a href="" className="header--button" target="_self">Conoce Nuestros Planes<span></span></a>
+        </div>
+      </header>
       <main>
         <section></section>
         <section></section>

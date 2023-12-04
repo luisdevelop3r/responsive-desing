50b3991048794baae53ccab22871a0d4c5862e0b
luisdevelop3r <luisvalientedeveloper@gmail.com>
2023-12-04 09:41:26 -0500

    tablas completadas y maquetacion de las targets

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,61 @@ function App() {
         </div>
       </header>
       <main>
-        <section></section>
-        <section></section>
+        <section className='main-exchange-container'>
+          <div className='backgroundImg'></div>
+          <div className='main-exchange-container--title'>
+            <h2>Visibilizamos todas las tazas de cambio.</h2>
+            <p>Traemos información en tiempo real de las casas de cambio y las monedas más importantes del mundo.</p>
+          </div>
+          <section className='main-tables-container'>
+            <div className='main-currency-table'>
+              <p className='currency-table--title'>Monedas</p>
+              <div className='currency-table--container'>
+                <p>Bitcoin</p>
+                <p>$ 1.96 <u></u></p>
+                <p>Ethereum</p>
+                <p>$ 0.07 <i></i></p>
+                <p>Ripple</p>
+                <p>$ 2.15 <u></u></p>
+                <p>Stellar</p>
+                <p>$ 4.96 <u></u></p>
+              </div>
+              <div className='currency-table--date'>
+                <p><b>Actualizado:</b> 19 de julio 23:45</p>
+              </div>
+            </div>
+            <div className='main-currency-table'>
+              <p className='currency-table--title blueTitle'>Comisiones</p>
+              <div className='currency-table--container'>
+                <p>Bitrade</p>
+                <p>$ 12.96</p>
+                <p>Bitpreco</p>
+                <p>$ 13.07</p>
+                <p>Novadax</p>
+                <p>$ 13.15</p>
+                <p>Coinext</p>
+                <p>$ 14.96</p>
+              </div>
+              <div className='currency-table--date blueDate'>
+                <p><b>Actualizado:</b> 19 de julio 23:48</p>
+              </div>
+            </div>
+          </section>
+        </section>
+        <section className='main-product-detail'>
+          <span className="product-detail--batata-logo"></span>
+          <div className="product-detail--title">
+            <h2>Creamos un producto sin comparación.</h2>
+            <p>Confiable y diseñado para su uso diario.</p>
+          </div>
+          <section className="product-cards--container">
+            <article className="product-detail--card">
+              <span className="clock"></span>
+              <p className="product--card-title">Tiempo real</p>
+              <p className="product--card-body">Nuestro API toma información minuto a minuto sobre las tasas que más determinan el comportamiento.</p>
+            </article>
+          </section>
+        </section>
         <section></section>
         <section></section>
       </main>
